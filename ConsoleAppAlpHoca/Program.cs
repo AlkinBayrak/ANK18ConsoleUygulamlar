3c97f8489961f34b2f1b168e8646664a81f8d552
AlkinBayrak <alkinbayrak01@gmail.com>
2024-07-04 17:41:07 +0300

    Derste Örnekler

--- ConsoleAppAlpHoca/Program.cs
+++ ConsoleAppAlpHoca/Program.cs
@@ -28,22 +28,80 @@
             //Console.WriteLine($"Yaşınız: {yas}  sayi: {s1}");
 
 
-            int s1, s2, s3, s4, s5;
+            //int s1, s2, s3, s4, s5;
 
-            Console.WriteLine("5 adet sayıyı giriniz");
+            //Console.WriteLine("5 adet sayıyı giriniz");
+
+            //s1 = Convert.ToInt32(Console.ReadLine());
+            //s2 = Convert.ToInt32(Console.ReadLine());
+            //s3 = Convert.ToInt32(Console.ReadLine());
+            //s4 = Convert.ToInt32(Console.ReadLine());
+            //s5 = Convert.ToInt32(Console.ReadLine());
+
+            //double islem = (s1+s2+s3+s4+s5)/5.0;
+
+            //Console.WriteLine($"Aritmetik ortalaması: {islem}");
+            //---------------------------------------------------
+            //int ö = 80;
+            //int y = 150;
+
+            //int ögrenci, yetiskin;
+
+            //Console.WriteLine("Öğrenci sayısını giriniz");
+
+            //ögrenci = Convert.ToInt32(Console.ReadLine());
+
+            //Console.WriteLine("Yetişkin sayısını giriniz");
+
+
+            //yetiskin = Convert.ToInt32(Console.ReadLine());
+
+            //int islem = ö*ögrenci + y*yetiskin;
+
+            //Console.WriteLine($"Hesaplanan tutar: {islem}");
+            //-----------------------------------------------
+
+            /* 
+             double yaricap, alan;
+
+             int yariccap;
+
+             double h = 22 / 7;
+
+             Console.WriteLine("Yarı çapı giriniz:");
+
+             yariccap = Convert.ToInt32(Console.ReadLine());
+
+             yaricap = 2 * h * yariccap;
+
+             alan = h * (yariccap * yariccap);
+
+             Console.WriteLine($"Yarı çapı : {yaricap}");
+
+             Console.WriteLine($"Alan : {alan}");
+             --------------------------------------*/
+
+
+            /*int dogumyili;
+
+             Console.WriteLine("Doğum yılı giriniz:");
+
+             dogumyili = Convert.ToInt32(Console.ReadLine());
+
+             int islem = 2024 - dogumyili;
+
+             Console.WriteLine($"Yaş: {islem}");*/
+
+            int s1, s2;
+
+            Console.WriteLine("İki adet sayı giriniz :");
 
             s1 = Convert.ToInt32(Console.ReadLine());
             s2 = Convert.ToInt32(Console.ReadLine());
-            s3 = Convert.ToInt32(Console.ReadLine());
-            s4 = Convert.ToInt32(Console.ReadLine());
-            s5 = Convert.ToInt32(Console.ReadLine());
 
-            double islem = (s1+s2+s3+s4+s5)/5.0;
-            
-            Console.WriteLine($"Aritmetik ortalaması: {islem}");
+            int islem = (s1*s1) + (s2*s2);
 
-
-
+            Console.WriteLine($"Karelerinin toplamı : {islem}");
         }
     }
 }
